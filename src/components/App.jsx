631a87dfebf365e3275d1f42a302c31c47cac35d
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2025-03-05 19:25:22 +0200

    add Sip Happens Cafe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,8 +4,58 @@ import friends from "../friends.json";
 import FriendList from "./FriendList/FriendList.jsx";
 import TransactionHistory from "./TransactionHistory/TransactionHistory.jsx";
 import transactions from "../transactions.json";
+import { useEffect, useState } from "react";
+import Options from "./Options/Options.jsx";
+import Feedback from "./Feedback/Feedback.jsx";
+import Notification from "./Notification/Notification.jsx";
 
 export default function App() {
+  // const [clicks, setClicks] = useState(() => {
+  //   const savedClicks = window.localStorage.getItem("saved-clicks");
+  //   if (savedClicks !== null) {
+  //     return Number(savedClicks);
+  //   }
+  //   return 0;
+  // });
+  // const [isOpen, setIsOpen] = useState(false);
+  // const [first, setFirst] = useState(false);
+  // const [second, setSecond] = useState(false);
+
+  // useEffect(() => {
+  //   console.log("First update: ", first);
+  // }, [first]);
+
+  // useEffect(() => {
+  //   console.log("Second update: ", second);
+  // }, [second]);
+
+  // useEffect(() => {
+  //   console.log("First or second update: ", first + second);
+  // }, [first, second]);
+
+  // useEffect(() => {
+  //   window.localStorage.setItem("saved-clicks", clicks);
+  // }, [clicks]);
+
+  // const handleClick = () => {
+  //   setClicks(clicks + 1);
+  // };
+
+  // const handleToggle = () => {
+  //   setIsOpen(!isOpen);
+  // };
+
+  const [feedback, setFeedback] = useState({ good: 0, neutral: 0, bad: 0 });
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevState) => ({
+      ...prevState,
+      [feedbackType]: prevState[feedbackType] + 1,
+    }));
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
   return (
     <>
       <Profile
@@ -17,6 +67,45 @@ export default function App() {
       />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
+      {/* <Button onUpdate={handleClick} value={clicks} />
+      <Button onUpdate={handleClick} value={clicks} /> */}
+      {/* <button onClick={handleToggle}>{isOpen ? "Close" : "Open"}</button>
+      {isOpen && <Modal />} */}
+      {/* <button onClick={handleClick}>You clicked {clicks} times</button> */}
+      {/* <button
+        onClick={() => {
+          setClicks(0);
+        }}
+      >
+        Reset
+      </button> */}
+      {/* <button
+        onClick={() => {
+          setFirst(first + 1);
+        }}
+      >
+        First: {first}
+      </button>
+      <button
+        onClick={() => {
+          setSecond(second + 1);
+        }}
+      >
+        Second: {second}
+      </button> */}
+      <div>
+        <h1>Sip Happens Café</h1>
+        <p>
+          Please leave your feedback about our service by selecting one of the
+          options below.
+        </p>
+        <Options updateFeedback={updateFeedback}></Options>
+        {totalFeedback > 0 ? (
+          <Feedback feedback={feedback} />
+        ) : (
+          <Notification />
+        )}
+      </div>
     </>
   );
 }
---
+++ src/components/Options/Options.jsx
@@ -0,0 +1,27 @@
+export default function Options({ updateFeedback }) {
+  return (
+    <>
+      <button
+        onClick={() => {
+          updateFeedback("good");
+        }}
+      >
+        Good
+      </button>
+      <button
+        onClick={() => {
+          updateFeedback("neutral");
+        }}
+      >
+        Neutral
+      </button>
+      <button
+        onClick={() => {
+          updateFeedback("bad");
+        }}
+      >
+        Bad
+      </button>
+    </>
+  );
+}
